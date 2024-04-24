99560539d26fc4f67af9e0e964fa7fbc6e49ef42
Motasem <motasemali0000@gmail.com>
2024-04-24 10:46:42 +0300

    Player information page logic is finished
    User should fill the inputs and select a symbol
    - if one is not selected/inputed user will be alerted to fill out the
      data

---
+++ app/player-info.js
@@ -0,0 +1,50 @@
+const username = document.getElementById("username");
+const numberOfRounds = document.getElementById("rounds");
+const meFirst = document.getElementById("user-first");
+const btnPlay = document.querySelector(".btnSubmit");
+const cardsContainer = document.querySelector(".cards");
+const cards = document.querySelectorAll(".card");
+
+cardsContainer.addEventListener("click", function (e) {
+  cards.forEach((card) => {
+    if (card.classList.contains("active")) {
+      card.classList.remove("active");
+    }
+  });
+  e.target.closest(".card").classList.add("active");
+});
+
+function oneCardActive() {
+  return Array.from(cards).some((card) => {
+    return card.classList.contains("active");
+  });
+}
+
+btnPlay.addEventListener("click", function (e) {
+  e.preventDefault();
+  const onsIsActive = oneCardActive();
+  if (
+    username.value !== "" &&
+    username.value !== null &&
+    numberOfRounds.value >= 1 &&
+    numberOfRounds.value <= 5 &&
+    onsIsActive
+  ) {
+    const playerSymbol = cardsContainer
+      .querySelector(".active")
+      .querySelector(".card__symbol").textContent;
+    const userData = {
+      username: username.value,
+      numberOfRounds: numberOfRounds.value,
+      playerSymbol: playerSymbol.toLowerCase(),
+      playerFirst: meFirst.checked,
+    };
+    // Store the data object in localStorage
+    localStorage.setItem("userData", JSON.stringify(userData));
+
+    window.location.href = "http://127.0.0.1:5500/html/index.html";
+    console.log(userData);
+  } else {
+    alert("Please Fill Your Information");
+  }
+});
